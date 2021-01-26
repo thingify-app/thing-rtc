e8a4c25b204836123a61e85d8187b924ff974048
thingify-admin <thingify@thingify.app>
2021-01-26 14:33:28 +1100

    Flesh out methods for sending/parsing data for signalling.

--- peer/src/peer.ts
+++ peer/src/peer.ts
@@ -2,11 +2,17 @@
 // abstract websocket
 
 export class SignallingServer {
+    private socket?: WebSocket;
 
     constructor(private options: SignallingOptions) {}
 
     async connect(token: string): Promise<SignallingConnection> {
-        throw new Error('Not implemented.');
+        return new Promise((resolve, reject) => {
+            this.socket = new WebSocket(this.options.serverHost);
+            this.socket.addEventListener('open', () => {
+                resolve(new InternalSignallingConnection(this.socket!, token));
+            });
+        });
     }
 }
 
@@ -21,6 +27,120 @@ export interface SignallingConnection {
     sendOffer(offer: RTCSessionDescriptionInit): void;
     sendAnswer(answer: RTCSessionDescriptionInit): void;
     disconnect(): void;
+}
+
+class InternalSignallingConnection implements SignallingConnection {
+    private peerConnectListener?: () => void;
+    private iceCandidateListener?: (candidate: RTCIceCandidate) => void;
+    private offerListener?: (offer: RTCSessionDescriptionInit) => void;
+    private answerListener?: (answer: RTCSessionDescriptionInit) => void;
+    private peerDisconnectListener?: () => void;
+    private errorListener?: () => void;
+
+    constructor(private socket: WebSocket, private token: string) {
+        socket.addEventListener('open', () => {
+            this.sendAuthMessage();
+        });
+        socket.addEventListener('message', event => {
+            this.handleMessage(event.data);
+        });
+        socket.addEventListener('error', () => {
+            this.errorListener?.();
+        });
+        socket.addEventListener('close', () => {
+            this.errorListener?.();
+        });
+    }
+
+    private sendAuthMessage() {
+        this.sendMessage({
+            type: 'auth',
+            token: this.token
+        });
+    }
+
+    private handleMessage(message: any) {
+        if (message && typeof(message) === 'string') {
+            const json = JSON.parse(message);
+            if (json && json.type) {
+                switch (json.type) {
+                    case 'peerConnect':
+                        this.peerConnectListener?.();
+                        break;
+                    case 'iceCandidate':
+                        this.iceCandidateListener?.(json.candidate);
+                        break;
+                    case 'offer':
+                        this.offerListener?.(json.offer);
+                        break;
+                    case 'answer':
+                        this.answerListener?.(json.answer);
+                        break;
+                    case 'peerDisconnect':
+                        this.peerDisconnectListener?.();
+                        break;
+                    default:
+                        throw new Error(`Unknown message type received: ${json.type}`);
+                }
+            } else {
+                throw new Error('Unknown message received');
+            }
+        }
+    }
+
+    on(type: string, callback: any) {
+        switch (type) {
+            case 'peerConnect':
+                this.peerConnectListener = callback;
+                break;
+            case 'iceCandidate':
+                this.iceCandidateListener = callback;
+                break;
+            case 'offer':
+                this.offerListener = callback;
+                break;
+            case 'answer':
+                this.answerListener = callback;
+                break;
+            case 'peerDisconnect':
+                this.peerDisconnectListener = callback;
+                break;
+            case 'error':
+                this.errorListener = callback;
+                break;
+            default:
+                throw new Error(`Unknown event type: ${type}`);
+        }
+    }
+
+    sendIceCandidate(candidate: RTCIceCandidate): void {
+        this.sendMessage({
+            type: 'iceCandidate',
+            candidate: candidate.toJSON()
+        });
+    }
+
+    sendOffer(offer: RTCSessionDescriptionInit): void {
+        this.sendMessage({
+            type: 'offer',
+            offer: offer
+        });
+    }
+
+    sendAnswer(answer: RTCSessionDescriptionInit): void {
+        this.sendMessage({
+            type: 'answer',
+            answer: answer
+        });
+    }
+
+    private sendMessage(data: any) {
+        this.socket.send(JSON.stringify(data));
+    }
+
+    disconnect(): void {
+        this.socket.close();
+    }
 }
 
 export interface SignallingOptions {
