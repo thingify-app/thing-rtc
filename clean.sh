041d0975512c46d59b02821291b2544ef3ff60c8
thingify-admin <thingify@thingify.app>
2021-01-30 08:46:41 +1100

    Add postinstall build to peer library, so that a clean npm install from example will build all necessary dependencies.

---
+++ clean.sh
@@ -0,0 +1,11 @@
+rm -r peer/node_modules
+rm -r peer/.cache
+rm -r peer/dist
+
+rm -r example/node_modules
+rm -r example/.cache
+rm -r example/dist
+
+rm -r server/node_modules
+rm -r server/.cache
+rm -r server/dist
